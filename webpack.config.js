4f9a08568849c92b35d57f00fa7dd5ec0e49cee8
giva16 <ltpie1@hotmail.com>
2024-03-20 15:20:52 +0100

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -44,5 +45,6 @@ module.exports = {
       template: './src/index.html',
     }),
     new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin('./src/assets/favicon.png'),
   ],
 };
